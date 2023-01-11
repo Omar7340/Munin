ce96b255aa0cce1e938f9446250ee8061095aae5
repo_distant_momomar <repo_distant_momomar@localhost>
2023-01-11 11:46:20 +0000

    + model scenarios

---
+++ src/scenarios/models.py
@@ -0,0 +1,22 @@
+{
+    "packets": [
+        {
+            "type": "periodique",
+            "vitesse": "55.6",
+            "geo": {
+                "lat": "43.174797",
+                "lon": "16.441231"
+            },
+            "immatriculation": "AA-123-BB"
+        },
+        {
+            "type": "ponctuelle",
+            "accident": "true",
+            "geo": {
+                "lat": "43.174797",
+                "lon": "16.441231"
+            },
+            "immatriculation": "AA-123-BB"
+        }
+    ]
+}
